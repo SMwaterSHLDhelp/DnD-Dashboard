682a4db762a44d369a043d7519e96c0a732d728b
Pilot AI <pilot-ai@localhost>
2026-03-27 18:30:05 -0400

    [Beelink Coder Next] Start: Create missing LootInventory component

---
+++ src/components/LootInventory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LootTracker from './LootTracker';
+
+const LootInventory = () => {
+  return (
+    <div className="loot-inventory">
+      <LootTracker />
+    </div>
+  );
+};
+
+export default LootInventory;
